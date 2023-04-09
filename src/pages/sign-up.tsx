ead7f5ab408e8e3a181ca11441f6727d479e7228
IgboPharaoh <solomoneze62@gmail.com>
2023-04-09 20:03:39 +0100

    add signup page

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <Box></Box>
+        </div>
+    );
+};
+
+export default SignUp;
