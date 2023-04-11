f9a38a471b3f1dc010cd804f0b40f22ac94ea3f5
IgboPharaoh <solomoneze62@gmail.com>
2023-04-11 04:23:36 +0100

    add header component

---
+++ src/pages/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Flex, Text, Link, HStack } from '@chakra-ui/react';
+import CustomButton from './CustomButton';
+import { useRouter } from 'next/router';
+
+const Header = (): JSX.Element => {
+    const router = useRouter();
+    return (
+        <Flex ml='80px' mr='80px' gap='64px' justifyContent='space-between'>
+            <HStack width='100%' >
+                <Text
+                    onClick={() => {
+                        router.push('/');
+                    }}
+                    fontSize='24px'
+                    fontWeight='600'
+                    color='#0000b3'
+                    cursor='pointer'
+                >
+                    Aegis
+                </Text>
+            </HStack>
+            <HStack width='100%' justifyContent='flex-end'>
+                <CustomButton onClick={() => {}} backgroundColor='#000066' width={{ base: '150px' }} height='48px'>
+                    Create Account
+                </CustomButton>
+            </HStack>
+        </Flex>
+    );
+};
+
+export default Header;
