ecbe97f75a671540a679dfbbf10f19d42565e6ac
IgboPharaoh <solomoneze62@gmail.com>
2023-04-21 17:28:03 +0100

    add RecieveSection component

--- src/pages/components/ReceiveSection.tsx
+++ src/pages/components/ReceiveSection.tsx
@@ -1,25 +1,51 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
-import QRCode from 'react-qr-code';
+import { useBitcoinContext } from '../contexts/bitcoin-context';
 
-export interface ReceiveSectionProps {
-    qrCode: string;
-    address: string;
-}
-
-const ReceiveSection = ({ qrCode, address }: ReceiveSectionProps) => {
+const ReceiveSection = ({ onClickTitle }: { onClickTitle: () => void }) => {
+    const { address } = useBitcoinContext();
     return (
-        <div>
-            <Text pb='16px' color='#2d2d2d' fontWeight='600' fontSize='32px'>
-                Deposit
-            </Text>
-            {qrCode ? (
+        <Box mt='48px'>
+            <Flex gap='8px'>
+                <Text
+                    opacity='0.7'
+                    w='fit-content'
+                    cursor='pointer'
+                    onClick={onClickTitle}
+                    pb='16px'
+                    color='#2d2d2d'
+                    fontWeight='600'
+                    fontSize='32px'
+                >
+                    Deposit
+                </Text>
+                <Text fontWeight='600' fontSize='32px'>
+                    /
+                </Text>
+                <Text
+                    textDecor='underline'
+                    w='fit-content'
+                    cursor='pointer'
+                    onClick={onClickTitle}
+                    pb='16px'
+                    color='#2d2d2d'
+                    fontWeight='600'
+                    fontSize='32px'
+                >
+                    Recieve
+                </Text>
+            </Flex>
+            <Text mb='16px'>You can use any of this addresses to recieve funds</Text>
+            <Box height='450px' overflow='scroll'>
                 <Box>
-                    <QRCode value={qrCode} height='256px' width='256px' />
-                    <Text>{address}</Text>
+                    {address.map((address, index) => (
+                        <Box mb='16px' p='8px' borderRadius='8px' border='1px solid #2d2d2d' key={index}>
+                            <Text>{address.address}</Text>
+                        </Box>
+                    ))}
                 </Box>
-            ) : null}
-        </div>
+            </Box>
+        </Box>
     );
 };
 
