033cd289fa7799ce13ff31f71b23621fa3a1ac2b
IgboPharaoh <solomoneze62@gmail.com>
2023-04-21 17:26:16 +0100

    add DepositSection component

--- src/pages/components/DepositSection.tsx
+++ src/pages/components/DepositSection.tsx
@@ -6,8 +6,9 @@ import { useDepositTransactionHook } from '../hooks/useDepositHook';
 
 export interface DepositTransactionProps {
     depositCallback: () => void;
+    onClickTitle: () => void;
 }
-const DepositTransaction = ({ depositCallback }: DepositTransactionProps) => {
+const DepositTransaction = ({ depositCallback, onClickTitle }: DepositTransactionProps) => {
     const { sendTransaction, setSendTransaction } = useDepositTransactionHook();
     const handleInputChange = useCallback(
         (event: ChangeEvent<HTMLInputElement>) => {
@@ -21,9 +22,35 @@ const DepositTransaction = ({ depositCallback }: DepositTransactionProps) => {
     return (
         <div>
             <Box pt='48px'>
-                <Text pb='16px' color='#2d2d2d' fontWeight='600' fontSize='32px'>
-                    Deposit
-                </Text>
+                <Flex gap='8px'>
+                    <Text
+                        textDecor='underline'
+                        w='fit-content'
+                        cursor='pointer'
+                        onClick={onClickTitle}
+                        pb='16px'
+                        color='#2d2d2d'
+                        fontWeight='600'
+                        fontSize='32px'
+                    >
+                        Deposit
+                    </Text>
+                    <Text fontWeight='600' fontSize='32px'>
+                        /
+                    </Text>
+                    <Text
+                        w='fit-content'
+                        cursor='pointer'
+                        onClick={onClickTitle}
+                        pb='16px'
+                        color='#2d2d2d'
+                        opacity='0.7'
+                        fontWeight='600'
+                        fontSize='32px'
+                    >
+                        Recieve
+                    </Text>
+                </Flex>
                 <Stack spacing='24px'>
                     <CustomInput
                         labelName='Address'
