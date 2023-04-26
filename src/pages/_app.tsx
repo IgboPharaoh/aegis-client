19fa86d4e092f967660c9048ca30e9280458ae27
IgboPharaoh <solomoneze62@gmail.com>
2023-04-26 03:44:39 +0100

    remove node connection context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Raleway } from 'next/font/google';
 import { BitcoinContextProvider } from './contexts/bitcoin-context';
-import { NodeConnectionContextProvider } from './contexts/node-connection';
 
 const raleway = Raleway({ subsets: ['latin'] });
 
@@ -12,9 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <ChakraProvider toastOptions={{ defaultOptions: { position: 'top-right' } }}>
             <main className={raleway.className}>
                 <BitcoinContextProvider>
-                    <NodeConnectionContextProvider>
                         <Component {...pageProps} />
-                    </NodeConnectionContextProvider>
                 </BitcoinContextProvider>
             </main>
         </ChakraProvider>
