e1fa2c39346a349ee2175bc83e049b4d8cebd261
IgboPharaoh <solomoneze62@gmail.com>
2023-04-26 03:40:46 +0100

    add header to signin component

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,11 +1,14 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React, { ChangeEvent, useCallback } from 'react';
+import Header from './components/Header';
 import InputSection from './components/InputSection';
 import PageDesign from './components/PageDesign';
 import { useSignInHook } from './hooks/useSignInHook';
 
 const SignIn = () => {
     const { loginData, setLoginData, email, password, error, isLoading, signInToAccount } = useSignInHook();
+    const router = useRouter();
 
     const handleInputChange = useCallback(
         (event: ChangeEvent<HTMLInputElement>) => {
@@ -18,6 +21,9 @@ const SignIn = () => {
 
     return (
         <div>
+            <Box mt='16px' width='100%' position='absolute'>
+                <Header onClick={() => router.push('/sign-up')} />
+            </Box>
             <Flex>
                 <InputSection
                     email={email}
